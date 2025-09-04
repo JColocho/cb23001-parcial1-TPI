7fd8cada24272afcf39996a3e89a25f7322beee1
JColocho <Jcolocho2003@gmail.com>
2025-09-04 12:53:31 -0600

    Vista inicial ejercicio3

---
+++ ejercicio3.php
@@ -0,0 +1,29 @@
+<?php 
+$compras = [
+    ['producto'=>'papas','precio'=> '0.50','cantidad'=> '10'],
+    ['producto'=>'tomate','precio'=> '0.60','cantidad'=> '5'],
+    ['producto'=>'camisa','precio'=> '7','cantidad'=> '2']
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+        </thead>
+        <tbody>
+            <tr>
+                
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
